4f30db9920b60a39b713162869d8978e34f38cc4
liushui <adcaoe@126.com>
2023-02-20 17:16:04 +0800

    local1

--- src/main/java/com/example/liushui/common/config/BaseRedisConfig.java
+++ src/main/java/com/example/liushui/common/config/BaseRedisConfig.java
@@ -4,9 +4,10 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.macro.mall.common.service.RedisService;
-import com.macro.mall.common.service.impl.RedisServiceImpl;
+import com.example.liushui.common.service.RedisService;
+import com.example.liushui.common.service.impl.RedisServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
@@ -23,6 +24,7 @@ import java.time.Duration;
  * Redis基础配置
  * Created by macro on 2020/6/19.
  */
+@Configuration
 public class BaseRedisConfig {
 
     @Bean
@@ -62,7 +64,7 @@ public class BaseRedisConfig {
 
     @Bean
     public RedisService redisService(){
-        return new RedisServiceImpl();
+        return (RedisService) new RedisServiceImpl();
     }
 
 }
